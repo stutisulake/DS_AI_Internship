d91386c4480cd2833925675f80f99cc13b100fa3
stutisulake <stutisulake1021@gmail.com>
2026-02-05 10:46:47 +0530

    Delete src/day3_collections.py/data_slicer.py

--- src/day3_collections.py/data_slicer.py
+++
@@ -1,6 +0,0 @@
-temperatures = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
-
-print("First Reading:", temperatures[0])
-print("Last Reading:", temperatures[-1])
-print("Afternoon Peak Readings:", temperatures[3:6])
-print("Last 3 Hours Readings:", temperatures[-3:])
